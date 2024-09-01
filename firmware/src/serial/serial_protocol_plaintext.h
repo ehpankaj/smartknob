e7768b1faf9179b4c3571d00f31775daff4389fb
Pankaj kumar Prajapat <ehpankaj@gmail.com>
2024-09-01 18:48:53 +0530

    Revert "change 1"
    This reverts commit 7e92dd6fea06a3c2e54235e8df048277c5c4a865.

--- firmware/src/serial/serial_protocol_plaintext.h
+++ firmware/src/serial/serial_protocol_plaintext.h
@@ -10,22 +10,20 @@
 typedef std::function<void(void)> DemoConfigChangeCallback;
 typedef std::function<void(void)> StrainCalibrationCallback;
 
-class SerialProtocolPlaintext : public SerialProtocol
-{
-public:
-    void handleNewConfigInput();
-    SerialProtocolPlaintext(Stream &stream, MotorCalibrationCallback motor_calibration_callback) : SerialProtocol(), stream_(stream), motor_calibration_callback_(motor_calibration_callback) {}
-    ~SerialProtocolPlaintext() {}
-    void log(const char *msg) override;
-    void loop() override;
-    void handleState(const PB_SmartKnobState &state) override;
+class SerialProtocolPlaintext : public SerialProtocol {
+    public:
+        SerialProtocolPlaintext(Stream& stream, MotorCalibrationCallback motor_calibration_callback) : SerialProtocol(), stream_(stream), motor_calibration_callback_(motor_calibration_callback) {}
+        ~SerialProtocolPlaintext(){}
+        void log(const char* msg) override;
+        void loop() override;
+        void handleState(const PB_SmartKnobState& state) override;
 
-    void init(DemoConfigChangeCallback demo_config_change_callback, StrainCalibrationCallback strain_calibration_callback);
-
-private:
-    Stream &stream_;
-    MotorCalibrationCallback motor_calibration_callback_;
-    PB_SmartKnobState latest_state_ = {};
-    DemoConfigChangeCallback demo_config_change_callback_;
-    StrainCalibrationCallback strain_calibration_callback_;
+        void init(DemoConfigChangeCallback demo_config_change_callback, StrainCalibrationCallback strain_calibration_callback);
+    
+    private:
+        Stream& stream_;
+        MotorCalibrationCallback motor_calibration_callback_;
+        PB_SmartKnobState latest_state_ = {};
+        DemoConfigChangeCallback demo_config_change_callback_;
+        StrainCalibrationCallback strain_calibration_callback_;
 };
